a9bcaccaba0b9c9ce42379ba037edc9be6cdb079
tpellizzari <theo.pellizzari2@ent.univ-lorraine.fr>
2024-12-11 14:58:45 +0100

    Initialise frontend avec Next.js et Tailwind CSS

---
+++ frontend/app/pages/index.js
@@ -0,0 +1,10 @@
+import Header from '../components/Header';
+
+export default function Home() {
+    return (
+        <div>
+            <Header />
+            <main className="p-4">Welcome to GameAscension!</main>
+        </div>
+    );
+}
---
+++ frontend/components/Header.js
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <header className="bg-blue-600 text-white p-4">
+            <h1 className="text-lg font-bold">GameAscension</h1>
+        </header>
+    );
+};
+export default Header;
